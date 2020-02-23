7fa0348ac8609371fec55e88dce38b88ab29fa07
Mohamed <25180044+MohamedSahbi@users.noreply.github.com>
2020-02-24 00:42:00 +0100

    Add ObjectExtensions

---
+++ DotNetToolkit/Extensions/ObjectExtensions.cs
@@ -0,0 +1,28 @@
+﻿
+namespace DotNetToolkit.Extensions
+{
+	public static class ObjectExtensions
+    {
+		public static bool IsNull(this object obj)
+		{
+			return obj == null;
+		}
+
+		public static bool IsNotNull(this object obj)
+		{
+			return !IsNull(obj);
+		}
+
+		public static bool TryCast<T>(this object obj, out T result)
+		{
+			if (obj is T)
+			{
+				result = (T)obj;
+				return true;
+			}
+
+			result = default;
+			return false;
+		}
+	}
+}

f356d2777e312f87efe6402751fc0545fa569ef2
Mohamed <25180044+MohamedSahbi@users.noreply.github.com>
2020-02-23 23:43:05 +0100

    Add ObjectComparer

---
+++ DotNetToolkit/Helpers/ObjectComparer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace DotNetToolkit.Helpers
+{
+    public static class ObjectComparer<T> where T : class
+    {
+
+        public static bool HasChanged(T originalObject, T changedObject)
+        {
+            bool result = false;
+
+            foreach (PropertyInfo property in originalObject.GetType().GetProperties())
+            {
+
+                object originalPropertyValue =
+                    property.GetValue(originalObject, null);
+                object newPropertyValue =
+                    property.GetValue(changedObject, null);
+
+                if (!Equals(originalPropertyValue, newPropertyValue))
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public static bool HasChanged(T originalObject, T changedObject, List<Type> typesToIgnore)
+        {
+            bool result = false;
+
+            foreach (PropertyInfo property in originalObject.GetType().GetProperties())
+            {
+
+                object originalPropertyValue =
+                    property.GetValue(originalObject, null);
+                object newPropertyValue =
+                    property.GetValue(changedObject, null);
+                if (originalPropertyValue != null && typesToIgnore.Contains(originalPropertyValue.GetType()))
+                {
+                    continue;
+                }
+                
+                if (!Equals(originalPropertyValue, newPropertyValue))
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}

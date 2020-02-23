bde022ef020f5d1e2e96f1cabcd4aed1ea69802b
Mohamed <25180044+MohamedSahbi@users.noreply.github.com>
2020-02-23 23:36:21 +0100

    Create StringExtensions and PaginatedList

---
+++ DotNetToolkit/Extensions/StringExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DotNetToolkit.Extensions
+{
+    public static class StringExtensions
+    {
+
+        public static bool IsEmptyString(this string text)
+        {
+            return string.IsNullOrEmpty(text);
+        }
+
+        public static bool IsNotEmptyString(this string text)
+        {
+            return !IsEmptyString(text);
+        }
+
+        /// <summary>
+        /// Returns a copy of this string without white spaces
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string RemoveWhiteSpaces(this string text)
+        {
+            text = text.Trim().Replace(" ", "");
+            return text;
+        }
+
+        public static string Truncate(this string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+        }
+
+
+        public static bool IsForbiddenWord(this string text)
+        {
+            bool result = false;
+
+            //Add the forbidden words here
+            string[] forbiddenWords = new string[] { "null", "nul", "object" };
+
+            text = text.ToLower().Trim();
+            if (Array.IndexOf(forbiddenWords, text) >= 0) result = true;
+
+            return result;
+        }
+
+    }
+}
